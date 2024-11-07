adfcd06bad96a4fbf971bb8da4323583a5f18910
Svess <TheSvess@gmail.com>
2024-11-07 12:03:53 +0000

    Changed Exp System

--- src/utils/calculateLevelExp.js
+++ src/utils/calculateLevelExp.js
@@ -4,15 +4,11 @@ Calculates the amount of Exp needed to go up a level
 
 /**
  * @param {Number} level
- * @return {Number} EXP neede to level up to input level.
+ * @return {Number} EXP needed to level up to input level.
  */
 
 module.exports = (level) => {
-  if (level < 15) {
-    return Math.floor((Math.pow(level, 3) * (Math.floor((level + 1) / 3) + 24)) / 40);
-  } else if (level >= 15 && level < 36) {
-    return Math.floor((Math.pow(level, 3) * (level + 14)) / 40);
-  } else {
-    return Math.floor((Math.pow(level, 3) * (Math.floor(level / 2) + 32)) / 40);
-  }
+  if (level < 15) return Math.ceil((Math.pow(level, 3) * (Math.floor((level + 1) / 3) + 24)) / 40);
+  if (level < 36) return Math.ceil((Math.pow(level, 3) * (level + 14)) / 40);
+  return Math.ceil((Math.pow(level, 3) * (Math.floor(level / 2) + 32)) / 40);
 };
